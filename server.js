e3e7ddd818ec963206077a60297ab1b61ab62a6d
Lucas <lucasbeta101@gmail.com>
2026-07-27 05:30:49 -0300

    fix(cors): allow localhost:5173 origin for dev environment

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(cors({
     // Desarrollo local
     'http://localhost:3000',
     'http://localhost:5000',
+    'http://localhost:5173',
     'http://127.0.0.1:5500',
     'http://localhost:8080'
   ],
